1810d493f58ab02f41a6d727fb3bc81264c00fdf
Nastia <gaman.anastasiia@gmail.com>
2021-10-08 18:51:42 +0300

    Task.java

--- ClassTask/Task.java
+++ ClassTask/Task.java
@@ -1,15 +1,18 @@
+package com.softserve;
+
 public class Task {
     private String title;
     private String type;
     private int priority;
-    private LocalDateTime dateTime;
-    //static int count = 0; - можна зробити таку штуку, щоб вона автоматично підраховувала кількість об'єктів
+   // private LocalDateTime dateTime;
 
     public String getTitle(){
+
         return title;
     }
 
     public void setTitle(String title){
+
         this.title = title;
     }
 
@@ -29,32 +32,45 @@ public class Task {
         this.priority = priority;
     }
 
-    public LocalDateTime getDateTime(){
+   /* public LocalDateTime getDateTime(){
         return dateTime;
     }
 
     public void setDateTime(){
         this.dateTime = dateTime;
-    }
-
-    public Task(String title, String types, int priority,LocalDateTime dateTime){
+    }*/
+    public Task(String title, String types, int priority/*LocalDateTime dateTime*/){
         this.title = title;
         this.type = types;
         this.priority = priority;
-        this.dateTime = dateTime;
+        //this.dateTime = dateTime;
     }
-
+    public Task(String taskString){
+       String[] split = taskString.split(" ");
+       this.title = split [0];
+       this.type = split [1];
+       this.priority = Integer.parseInt(split[2]);
+       //this.dateTime = dateTime (split[3]);
+    }
+    public String toString(){
+        return title + " " + type + " " + priority;
+    }
     public void getTaskDescription(){
         System.out.println("Task title: " + title);
         System.out.println("Task type: " + type);
         System.out.println("Priority: " + priority);
     }
-    public String toString() {
+    public static void main (String[]args){
+        Task task1 = new Task("Title Type 3");
+        System.out.println (task1.toString());
+    }
+
+   /* public String toString() {
         return "Task{" +
                 "title='" + title + '\'' +
                 ", type='" + type + '\'' +
                 ", priority=" + priority +
                 ", dateTime=" + dateTime +
                 '}';
-    }
+    }*/
 }
---
+++ src/com/softserve/Task.java
@@ -0,0 +1,2 @@
+package com.softserve;public class Task {
+}
