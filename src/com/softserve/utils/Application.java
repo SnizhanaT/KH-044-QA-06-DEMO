350f9abd0a796f65e67a5575a3bf374e426f56e3
Snizhana Tretiak <sn.piatash@gmail.com>
2021-10-08 21:46:49 +0300

    Modified file Application.java

--- src/com/softserve/utils/Application.java
+++ src/com/softserve/utils/Application.java
@@ -1,25 +1,16 @@
 package com.softserve.utils;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static com.softserve.utils.FileUtils.*;
 
 public class Application {
 
     public static void main(String[] args) {
-        File taskFile = new File(taskFileName);
-        if (taskFile.exists()) {
-            System.out.println("Tasks file exists: " + taskFile.getName());
-                readFile(taskFileName);
-        } else createIfNotExists(taskFileName);
+        createIfNotExists(taskFileName);
+        readFile(taskFileName);
 
-            File deletedTaskFile = new File(deletedTaskFileName);
-            if (deletedTaskFile.exists()) {
-                System.out.println("Deleted tasks file exists: " + deletedTaskFile.getName());
-                readFile(deletedTaskFileName);
-            } else createIfNotExists(deletedTaskFileName);
-            }
+        createIfNotExists(deletedTaskFileName);
+        readFile(deletedTaskFileName);
     }
+}
