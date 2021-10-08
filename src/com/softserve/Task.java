8b90a532597b4d399a805973be276021d5aff164
Nastia <gaman.anastasiia@gmail.com>
2021-10-08 19:17:47 +0300

    Task.java

--- src/com/softserve/Task.java
+++ src/com/softserve/Task.java
@@ -1,2 +1,76 @@
-package com.softserve;public class Task {
+package com.softserve;
+
+public class Task {
+    private String title;
+    private String type;
+    private int priority;
+   // private LocalDateTime dateTime;
+
+    public String getTitle(){
+
+        return title;
+    }
+
+    public void setTitle(String title){
+
+        this.title = title;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String types){
+        this.type = type;
+    }
+
+    public int getPriority(){
+        return priority;
+    }
+
+    public void setPriority(int priority){
+        this.priority = priority;
+    }
+
+   /* public LocalDateTime getDateTime(){
+        return dateTime;
+    }
+
+    public void setDateTime(){
+        this.dateTime = dateTime;
+    }*/
+    public Task(String title, String types, int priority/*LocalDateTime dateTime*/){
+        this.title = title;
+        this.type = types;
+        this.priority = priority;
+        //this.dateTime = dateTime;
+    }
+    public Task(String taskString){
+       String[] split = taskString.split(" ");
+       this.title = split [0];
+       this.type = split [1];
+       this.priority = Integer.parseInt(split[2]);
+       //this.dateTime = dateTime (split[3]);
+    }
+    public String toString(){
+        return title + " " + type + " " + priority;
+    }
+    public void getTaskDescription(){
+        System.out.println("Task title: " + title);
+        System.out.println("Task type: " + type);
+        System.out.println("Priority: " + priority);
+    }
+    public static void main (String[]args){
+        Task task1 = new Task("Title Type 3");
+        System.out.println (task1.toString());
+    }
+
+   /* public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", type='" + type + '\'' +
+                ", priority=" + priority +
+                ", dateTime=" + dateTime +
+                '}';
+    }*/
 }
