29e563c1ddc78de647bd29208641b918283f76cf
Olha Popova <gurtovayaolenka@gmail.com>
2021-10-10 15:51:49 +0300

    method LocalDateTime into String - in process

--- src/com/softserve/utils/DateUtils.java
+++ src/com/softserve/utils/DateUtils.java
@@ -1,21 +1,32 @@
 package com.softserve.utils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DateUtils {
 
-    public final static String dateTimePattern = "dd-MM-yyyy HH:mm:ss";
-    public static String userInput = "07-10-2021 12:00:00";
-    public static String dateTime;
+    public final static String dateTimePattern = "dd-MM-yyyy HH:mm";
 
-/*  public static LocalDateTime parseFromString (String dateTime) {
-        LocalDateTime localDateTime = LocalDateTime.parse(userInput);
+    //parsing from String;
+    public static LocalDateTime parseFromString(String dateTime) {
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime);
         return localDateTime;
     }
 
-    public static String dateTimeToString (LocalDateTime localDateTime) {
-        String dateTime;
-        LocalDateTime.of();
-        return dateTime;
-    }*/
+    //output for user;
+    public static LocalDateTime dateTimeToString(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateTimePattern);
+        String formattedDateTime = localDateTime.format(formatter);
+        //return formattedDateTime;  // тут не совсем понимаю почему Идея просит использовать метод, который на след строке:
+        return LocalDateTime.parse(formattedDateTime);
+    }
+
+    public static void main(String[] args) throws Exception {
+        LocalDateTime localDateTime = parseFromString("2021-11-23T13:43:15");
+        System.out.println(localDateTime);
+
+        System.out.println(dateTimeToString(localDateTime));
+    }
 }
 
 
