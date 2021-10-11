69aa052fe7545fb4b89f8d2b7aeb2ed5a6c2bb66
Olha Popova <gurtovayaolenka@gmail.com>
2021-10-11 12:40:09 +0300

    were edited DateTimeFormatter for correct input of date from 2 methods

--- src/com/softserve/utils/DateUtils.java
+++ src/com/softserve/utils/DateUtils.java
@@ -5,18 +5,26 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
 
-    public final static String dateTimePattern = "dd-MM-yyyy HH:mm";
+    public final static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
     //parsing from String;
     public static LocalDateTime parseFromString(String dateTime) {
-        LocalDateTime localDateTime = LocalDateTime.parse(dateTime);
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, dateTimeFormatter);
         return localDateTime;
     }
 
     //output for user;
     public static String dateTimeToString(LocalDateTime localDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateTimePattern);
-        String formattedDateTime = localDateTime.format(formatter);
+        String formattedDateTime = localDateTime.format(dateTimeFormatter);
         return formattedDateTime;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+        String str = dateTimeToString(localDateTime);
+        System.out.println(str);
+        LocalDateTime dateTime = parseFromString(str);
+        System.out.println(dateTimeToString(dateTime));
     }
 }
