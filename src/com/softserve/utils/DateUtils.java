eb04b2c5210bba928bd14c33ba5b5bb88a05eb5f
Olha Popova <gurtovayaolenka@gmail.com>
2021-10-11 12:53:10 +0300

    updating editing of methods after conflicts

--- src/com/softserve/utils/DateUtils.java
+++ src/com/softserve/utils/DateUtils.java
@@ -17,17 +17,5 @@ public class DateUtils {
     public static String dateTimeToString(LocalDateTime localDateTime) {
         String formattedDateTime = localDateTime.format(dateTimeFormatter);
         return formattedDateTime;
-<<<<<<< HEAD
-    }
-
-    public static void main(String[] args) {
-        LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println(localDateTime);
-        String str = dateTimeToString(localDateTime);
-        System.out.println(str);
-        LocalDateTime dateTime = parseFromString(str);
-        System.out.println(dateTimeToString(dateTime));
-=======
->>>>>>> 4a8dbc144bd037397b734487a653e8a41fe5d33d
     }
 }
