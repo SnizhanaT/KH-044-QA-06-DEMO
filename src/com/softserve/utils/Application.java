6c737018016aaf2b8f37af79694eb3419fdf6281
Snizhana Tretiak <sn.piatash@gmail.com>
2021-10-14 19:33:09 +0300

    updated

--- src/com/softserve/utils/Application.java
+++ src/com/softserve/utils/Application.java
@@ -20,9 +20,43 @@ public class Application {
 
     static void editTask() {
         //TODO
+        //TO DO;
     }
 
     static void deleteTask() {
+        System.out.println("This is delete task menu.");
+        int option;
+        do {
+            System.out.println("Choose option: \n1 - Choose task that needs to be deleted \n-1 - Go back to main menu");
+            option = getInt();
+        }
+        while (option != 1 & option != -1);
+
+        switch (option) {
+            case 1:
+                System.out.println("View tasks and input id of the task that needs to be deleted:");
+                for (int i = 0; i < deletedTasksList.size(); i++) {
+                    System.out.println("Task id: " + i);
+                    deletedTasksList.get(i).getPrintTaskDescription();
+                }
+                int taskIndex;
+                do {
+                    System.out.println("Input task id");
+                    taskIndex = getInt();
+                }
+                while (taskIndex > deletedTasksList.size() & taskIndex <= 0);
+                System.out.println("Deleting task " + tasksList.get(taskIndex));
+                Task deletedTask = tasksList.remove(taskIndex);
+                deletedTask.setDeletedTime(LocalDateTime.now());
+                System.out.println("Successfully deleted");
+                writeFile(deletedTaskFileName, deletedTasksList);
+                tasksList.add(deletedTask);
+                writeFile(taskFileName, tasksList);
+                break;
+            case -1:
+                System.out.println("Go back to main menu");
+                break;
+        }
         //TODO
     }
 
