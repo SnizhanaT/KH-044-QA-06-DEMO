fbcba7000f1f6b71136cf1ca7729036a2c92a640
Snizhana Tretiak <sn.piatash@gmail.com>
2021-10-14 18:03:48 +0300

    Added validation for incorrect numbers input

--- src/com/softserve/utils/Application.java
+++ src/com/softserve/utils/Application.java
@@ -2,7 +2,6 @@ package com.softserve.utils;
 
 import com.softserve.Task;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.softserve.utils.ConsoleUtils.getInt;
@@ -21,34 +20,39 @@ public class Application {
     }
 
     static void deleteTask() {
-        System.out.println("This is delete task menu \nChoose option: ");
-        System.out.println("1 - Choose task that needs to be deleted \n-1 - Go back to main menu");
-        int option = getInt();
-        if (option != 1 && option != -1) {
-            System.out.println("Choose only 1 or -1");
+        System.out.println("This is delete task menu.");
+        int option;
+        do {
+            System.out.println("Choose option: \n1 - Choose task that needs to be deleted \n-1 - Go back to main menu");
+            option = getInt();
         }
-            switch (option) {
-                case 1:
-                    System.out.println("View tasks and input id of the task that needs to be deleted:");
-                    for (int i = 0; i < deletedTasksList.size(); i++) {
-                        System.out.println("Task id: " + i);
-                        deletedTasksList.get(i).printTaskDescription();
-                    }
-                    int taskIndex = getInt();
-                    System.out.println("Deleting task " + tasksList.get(taskIndex));
-                    Task deletedTask = tasksList.remove(taskIndex);
-                    deletedTask.setDeletedDateTime();
-                    System.out.println("Successfully deleted");
-                    writeFile(deletedTaskFileName, deletedTasksList);
-                    tasksList.add(deletedTask);
-                    writeFile(taskFileName, tasksList);
-                    break;
-                case -1:
-                    System.out.println("Go back to main menu");
-                    break;
-                default:
-                    System.out.println("Invalid option");
-            }
+        while (option != 1 & option != -1);
+
+        switch (option) {
+            case 1:
+                System.out.println("View tasks and input id of the task that needs to be deleted:");
+                for (int i = 0; i < deletedTasksList.size(); i++) {
+                    System.out.println("Task id: " + i);
+                    deletedTasksList.get(i).printTaskDescription();
+                }
+                int taskIndex;
+                do {
+                    System.out.println("Input task id");
+                    taskIndex = getInt();
+                }
+                while (taskIndex > deletedTasksList.size() & taskIndex <= 0);
+                System.out.println("Deleting task " + tasksList.get(taskIndex));
+                Task deletedTask = tasksList.remove(taskIndex);
+                deletedTask.setDeletedDateTime();
+                System.out.println("Successfully deleted");
+                writeFile(deletedTaskFileName, deletedTasksList);
+                tasksList.add(deletedTask);
+                writeFile(taskFileName, tasksList);
+                break;
+            case -1:
+                System.out.println("Go back to main menu");
+                break;
+        }
     }
 
     static void viewTasks() {
